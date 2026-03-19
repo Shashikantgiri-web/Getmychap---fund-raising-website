2e4e13b30b1722e0e076c55817ba94d5d11e8e9e
uselogin <Shashikantshankar707@gmail.com>
2026-03-19 08:04:46 +0530

    Update Navbar.js

--- main-code/components/Navbar.js
+++ main-code/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   const { data: session } = useSession()
   const [showdropdown, setShowDropdown] = useState(false);
   return (
-    <>
+    <div className='fixed z-20 top-0 start-0'>
       <div className='w-[99%] h-[10vh] hidden md:flex items-center justify-center bg-gray-900'>
         <div className='w-[30%] h-[99%] flex flex-row items-center justify-start'>
           <Link href="/" className='flex flex-row items-center justify-start'>
@@ -60,40 +60,40 @@ const Navbar = () => {
         <div className='w-[80%] h-[99%] flex items-center justify-end'>
           {session && <>
             <div className='relative flex flex-row items-center justify-center'>
-              <button className="flex justify-center items-center text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 p-0.5">
-                {session.user.image && <img src={session.user.image} alt="profile" className='w-6 h-6 rounded-full me-1' />}
+              <button className="flex items-center text-heading bg-transparent box-border border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none">
+                {session.user.image && <img src={session.user.image} alt="profile" className='w-7 h-7 rounded-full me-1' />}
               </button>
               <button data-collapse-toggle="mega-menu-full" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-lg md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-default" aria-controls="mega-menu-full" aria-expanded="false">
                 <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" /></svg>
               </button>
-            </div>
-            <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-              <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
-                <li>
-                  <Link href="#" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0" aria-current="page">Home</Link>
-                </li>
-                <li>
-                  <Link href="/Explore" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Explore</Link>
-                </li>
-                <li>
-                  <Link href="/contact" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact</Link>
-                </li>
-                <li>
-                  <Link href="/dashboard" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Dashboard</Link>
-                </li>
-                <li>
-                  <Link href={`/${session.user.name}`} class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Your Page</Link>
-                </li>
-                <li onClick={() => { signOut() }}>
-                  <a href='#' class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Log out</a>
-                </li>
-              </ul>
+              <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+                <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
+                  <li>
+                    <Link href="#" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0" aria-current="page">Home</Link>
+                  </li>
+                  <li>
+                    <Link href="/Explore" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Explore</Link>
+                  </li>
+                  <li>
+                    <Link href="/contact" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact</Link>
+                  </li>
+                  <li>
+                    <Link href="/dashboard" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link href={`/${session.user.name}`} class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Your Page</Link>
+                  </li>
+                  <li onClick={() => { signOut() }}>
+                    <a href='#' class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Log out</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </>}
           {!session && <Link href="/login"><button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 p-0.5">Login</button></Link>}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
