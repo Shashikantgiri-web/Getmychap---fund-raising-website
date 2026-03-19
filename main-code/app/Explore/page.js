33334407d1c09a0c6ba84cea79adce3ee0220070
uselogin <Shashikantshankar707@gmail.com>
2026-03-19 08:48:17 +0530

    fixing

--- main-code/app/Explore/page.js
+++ main-code/app/Explore/page.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import connectDB from '@/lib/db'
 
-const Explore = () => {
+const Explore = async () => {
+  const user = await connectDB();
+  console.log(user);
   return (
     <div className='w-[99%] h-[84vh] flex items-start justify-center'>all user data display</div>
   )
--- main-code/components/Navbar.js
+++ main-code/components/Navbar.js
@@ -67,28 +67,28 @@ const Navbar = () => {
               <button data-collapse-toggle="mega-menu-full" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-lg md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-default" aria-controls="mega-menu-full" aria-expanded="false" onClick={() => { const menu = document.getElementById('mega-menu-full'); if (menu.classList.contains('hidden')) { menu.classList.remove('hidden'); } else { menu.classList.add('hidden'); } }}>
                 <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" /></svg>
               </button>
-              <div id="mega-menu-full" className={`z-10 ${megamenufull ? '' : 'hidden'} absolute right-0 flex justify-center items-center text-white bg-gray-900 shadow-xs shadow-blue-700 font-medium rounded-2xl text-sm px-4 py-2.5 text-center leading-5 p-0.5 mt-2.5`}>
-                <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
-                  <li>
-                    <Link href="#" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0" aria-current="page">Home</Link>
-                  </li>
-                  <li>
-                    <Link href="/Explore" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Explore</Link>
-                  </li>
-                  <li>
-                    <Link href="/contact" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact</Link>
-                  </li>
-                  <li>
-                    <Link href="/dashboard" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link href={`/${session.user.name}`} class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Your Page</Link>
-                  </li>
-                  <li onClick={() => { signOut() }}>
-                    <a href='#' class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Log out</a>
-                  </li>
-                </ul>
-              </div>
+            </div>
+            <div id="mega-menu-full" className={`z-10 ${megamenufull ? '' : 'hidden'} absolute right-0 flex justify-center items-center text-white bg-gray-900 shadow-xs shadow-blue-700 font-medium rounded-2xl text-sm px-4 py-2.5 text-center leading-5 p-0.5 mt-2.5`}>
+              <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
+                <li>
+                  <Link href="#" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0" aria-current="page">Home</Link>
+                </li>
+                <li>
+                  <Link href="/Explore" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Explore</Link>
+                </li>
+                <li>
+                  <Link href="/contact" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact</Link>
+                </li>
+                <li>
+                  <Link href="/dashboard" class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Dashboard</Link>
+                </li>
+                <li>
+                  <Link href={`/${session.user.name}`} class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Your Page</Link>
+                </li>
+                <li onClick={() => { signOut() }}>
+                  <a href='#' class="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Log out</a>
+                </li>
+              </ul>
             </div>
           </>}
           {!session && <Link href="/login"><button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 p-0.5">Login</button></Link>}
