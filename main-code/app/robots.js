8cd8da76a5fbb274d3e3d64046247dbff9a9f35e
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-06-07 03:37:47 +0000

    add robots.js

---
+++ main-code/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap:
+      'https://getmychap-fund-raising-website.vercel.app/sitemap.xml',
+  };
+}
